34d063c77c290d46141d4c7ab5f49f3bc0607cb0
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2021-06-02 10:00:38 +0300

    Clean the demo script.

--- demo2.py
+++ demo2.py
@@ -11,39 +11,41 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 
 logger = setup_logger()
 
-INPUT = "IMG_8950.jpg"
+INPUT = "input.jpg"
+OUTPUT = "out.jpg"
+
+DEVICE = "cpu"
+MODEL_CONFIG = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
 
 
-def run():
+def predict():
     cfg = get_cfg()
-    cfg.merge_from_file(
-        model_zoo.get_config_file(
-            "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-        )
-    )
+    cfg.merge_from_file(model_zoo.get_config_file(MODEL_CONFIG))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-    )
-    cfg.MODEL.DEVICE = "cpu"
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(MODEL_CONFIG)
+    cfg.MODEL.DEVICE = DEVICE
+
     predictor = DefaultPredictor(cfg)
 
     im = cv2.imread(INPUT)
     outputs = predictor(im)
+
     outputs_instances = outputs["instances"]
     logger.info(f"Found {len(outputs_instances)} instances")
 
     dataset = cfg.DATASETS.TRAIN[0]
     logger.info("Dataset %s,", dataset)
 
+    # Find catalog for visualization purposes (what are the classes etc.)
     catalog = MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
     logger.info("Catalog %s", catalog)
+
     v = Visualizer(im[:, :, ::-1], catalog, scale=1.2)
     out = v.draw_instance_predictions(outputs["instances"])
-    OUTPUT = "out.jpg"
 
+    logger.info("Writing to file: %s", OUTPUT)
     cv2.imwrite(OUTPUT, out.get_image()[:, :, ::-1])
 
 
 if __name__ == "__main__":
-    run()
+    predict()
